128cae7d87635a02edb7a87eb17144f1a4222ce5
gamboaesai <70191163+gamboaesai@users.noreply.github.com>
2025-05-21 16:31:16 -0700

    Update firebase_client_config.py

--- firebase_client_config.py
+++ firebase_client_config.py
@@ -1,25 +1,7 @@
-# firebase_client_config.py
+import firebase_admin
+from firebase_admin import credentials, firestore
 
-from firebase_admin import credentials, firestore, initialize_app
-
-cred = credentials.Certificate("firebase_config.json")
-initialize_app(cred)
+cred = credentials.Certificate("path/to/your/firebase_config.json")
+firebase_admin.initialize_app(cred)
 db = firestore.client()
-
-import pyrebase
-
-firebase_config = {
-    "apiKey": "YOUR_API_KEY",
-    "authDomain": "your-app.firebaseapp.com",
-    "projectId": "your-app",
-    "storageBucket": "your-app.appspot.com",
-    "messagingSenderId": "SENDER_ID",
-    "appId": "APP_ID",
-    "databaseURL": "",
-}
-
-firebase = pyrebase.initialize_app(firebase_config)
-auth = firebase.auth()
-
-
 
