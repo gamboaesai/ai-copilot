fd762b3bd187886334013c8d2524f9b173249f41
gamboaesai <70191163+gamboaesai@users.noreply.github.com>
2025-05-21 19:28:31 -0700

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,13 +1,12 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import json
+import streamlit as st
 
-# Path to your Firebase service account key JSON
-SERVICE_ACCOUNT_PATH = "firebase_config.json"
-
-# Initialize Firebase app only once
 if not firebase_admin._apps:
-    cred = credentials.Certificate(SERVICE_ACCOUNT_PATH)
+    # Load from Streamlit secrets.toml
+    cred_dict = json.loads(st.secrets["firebase_config"])
+    cred = credentials.Certificate(cred_dict)
     firebase_admin.initialize_app(cred)
 
-# Create Firestore client
 db = firestore.client()
